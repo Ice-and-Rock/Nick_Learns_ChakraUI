577e00dcee0bd20320ef487514291e75c17bd8d4
nick v <nvalente.electrics@gmail.com>
2023-07-18 16:25:57 +0100

    added a profeil page to test a few different components and icon imports

--- Dojo_Tasks/src/pages/Profile.jsx
+++ Dojo_Tasks/src/pages/Profile.jsx
@@ -6,23 +6,36 @@ import {
   CardHeader,
   IconButton,
   Text,
-} from "@chakra-ui/react";
+} from "@chakra-ui/react"
 
 export default function Profile() {
   return (
     <Card bg="brand.200">
       <CardHeader>User Profile</CardHeader>
-      <CardBody display="flex" alignItems="center" bg="brand.300" margin="10px" borderRadius={8}>
+
+      <CardBody display="flex" alignItems="center" bgGradient="linear(to-b, brand.300, brand.350)" margin="10px" borderRadius={8}>
+        <Text fontWeight="bold" flexBasis="100px">Name:</Text>
         <Text flex="1">Nick Valente</Text>
         <Box display="flex" alignItems="center" justifyContent="center">
-          <EditIcon marginRight={3}/>
+          <EditIcon marginRight={3} />
           <DeleteIcon />
         </Box>
       </CardBody>
-      <CardBody display="flex" alignItems="center" bg="brand.300" margin="10px" borderRadius={8}>
-        <Text flex="1">Nick Valente</Text>
+
+      <CardBody display="flex" alignItems="center" bgGradient="linear(to-b, brand.300, brand.350)" margin="10px" borderRadius={8}>
+        <Text fontWeight="bold" flexBasis="100px">Email:</Text>
+        <Text flex="1">nvalente.electrics@gmail.com</Text>
         <Box display="flex" alignItems="center" justifyContent="center">
-          <EditIcon marginRight={3}/>
+          <EditIcon marginRight={3} />
+          <DeleteIcon />
+        </Box>
+      </CardBody>
+
+      <CardBody display="flex" alignItems="center" bgGradient="linear(to-b, brand.300, brand.350)" margin="10px" borderRadius={8}>
+        <Text fontWeight="bold" flexBasis="100px">Location:</Text>
+        <Text flex="1">Edinbrugh City, with a bonnie wee lass</Text>
+        <Box display="flex" alignItems="center" justifyContent="center">
+          <EditIcon marginRight={3} />
           <DeleteIcon />
         </Box>
       </CardBody>
